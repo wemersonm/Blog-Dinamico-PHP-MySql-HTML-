7ec84984a276afd1b3e77d5ac9b8bf05f0a03519
Wemerson <73254098+wemersonm@users.noreply.github.com>
2022-10-24 22:00:54 -0300

    Update criarCategoria.php

--- criarCategoria.php
+++ criarCategoria.php
@@ -1,10 +1,8 @@
 <?php
 
 ?>
-
 <h1>Criar Categoria</h1>
 <form action="insertCategoria.php" method="POST">
-
     <div>
         <input type="text" name="nome" placeholder="Nome da categoria"><br>
     </div>
